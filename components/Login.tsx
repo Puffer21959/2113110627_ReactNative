d08d67289c5898ad444faad67b143406d8a2fa8e
Puffer21959 <21959@tun.ac.th>
2024-08-10 09:57:11 +0700

    Additional Practice 1

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,13 +1,82 @@
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
-import React from "react";
+import { Alert, Button, StyleSheet, Text, TextInput, View } from "react-native";
+import React, { useState } from "react";
 import { stylesLogin } from "../styles/styles";
 
 const Login = (): React.JSX.Element => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [passsword, setPassword] = useState("");
+
+  const validateEmail = (email: string): boolean => {
+    const recheckEmail = /\S+@\S+\.\S+/;
+
+    console.log(/\S+@\S+\.S+/);
+    return recheckEmail.test(email); // regex
+    // \S+ = any 1 or more character that not empty space
+    // @ = find "@" character
+    // \. = find "." character
+    // start regex with "/" and end with "/"
+  };
+
+  const handleSubmit = () => {
+    //console.log("test");
+
+    let errorMessage = "";
+
+    if (!name) {
+      //console.log("name null");
+      //Alert.alert("Alert", "Please Enter Name", [{ text: "OK" }]);
+      //return;
+      errorMessage += "Please Enter Name.\n";
+    }
+    if (!email) {
+      //Alert.alert("Alert", "Please Enter Email", [{ text: "OK" }]);
+      //return;
+      errorMessage += "Please Enter Email.\n";
+    } else if (!validateEmail(email)) {
+      errorMessage += "Invalid Email Format.\n";
+    }
+
+    if (!passsword) {
+      errorMessage += "Please Enter Password.\n";
+    } else if (passsword.length < 6) {
+      errorMessage += "Password must be at least 6 character.\n";
+    }
+
+    if (errorMessage) {
+      Alert.alert("Error", errorMessage.trim(), [{ text: "OK" }]);
+      return;
+    }
+
+    //console.log("Success");
+    Alert.alert("Alert", "Success", [{ text: "OK" }]);
+  };
+
   return (
     <View style={stylesLogin.container}>
-      <TextInput style={stylesLogin.input} placeholder="Enter name" />
-      <TextInput style={stylesLogin.input} placeholder="Enter email" />
-      <Button title="Submit" />
+      <TextInput
+        style={stylesLogin.input}
+        placeholder="Enter Name"
+        value={name}
+        onChangeText={setName}
+      />
+      <TextInput
+        style={stylesLogin.input}
+        placeholder="Enter Email"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+      />
+      <TextInput
+        style={stylesLogin.input}
+        placeholder="Enter Passsword"
+        value={passsword}
+        onChangeText={setPassword}
+        secureTextEntry={true}
+      />
+      <View>
+        <Button title="Submit" onPress={handleSubmit} />
+      </View>
     </View>
   );
 };
